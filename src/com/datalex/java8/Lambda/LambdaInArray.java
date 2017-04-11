ec695258f2ef385ae95df99dd09c9b78978ca009
shaojie.xu <shaojie.xu@datalex.com>
2017-04-11 11:20:59 +0100

    refactoring into a method and another another method for comparator example

--- src/com/datalex/java8/Lambda/LambdaInArray.java
+++ src/com/datalex/java8/Lambda/LambdaInArray.java
@@ -1,6 +1,8 @@
 package com.datalex.java8.Lambda;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.function.Consumer;
 
@@ -11,7 +13,36 @@ public class LambdaInArray {
 
     public static void main(String... args) {
 
-        List<String> names = Arrays.asList("peter", "anna", "mike", "luna");
+        List<String> names = Arrays.asList("peter", "anna", "mike", "luna", "jacob", "angela");
+
+        LambdaInArray.testLambdaWithConsumer(names);
+        LambdaInArray.testLambdaWithComparator(names);
+
+    }
+
+    private static void testLambdaWithComparator(List<String> names) {
+
+        Collections.sort(names, new Comparator<String>() {
+            @Override
+            public int compare(String a, String b) {
+                return b.compareTo(a);
+            }
+        });
+
+        Collections.sort(names, (String a, String b) -> {
+            return b.compareTo(a);
+        });
+
+        Collections.sort(names, (String a, String b) -> b.compareTo(a));
+
+        Collections.sort(names, (a, b) -> b.compareTo(a));
+
+        Collections.sort(names, Comparator.reverseOrder());
+
+    }
+
+
+    private static void testLambdaWithConsumer(List<String> names) {
 
         // java 7 code
         for (String name : names)
@@ -25,15 +56,14 @@ public class LambdaInArray {
             }
         });
 
-        // java 8 code with forEach with lambda with type
+        // java 8 code with lambda
         names.forEach((String name) -> System.out.println(name));
 
-        // java 8 code with forEach with lambda without type
+        // java 8 code with lambda
         names.forEach(name -> System.out.println(name));
 
-        // java 8 code with forEach with method reference
+        // java 8 code with method reference
         names.forEach(System.out::println);
-
 
     }
 
